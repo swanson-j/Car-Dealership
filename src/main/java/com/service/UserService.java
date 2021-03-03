46e98529756e07bedd1066574c639d252c62b45f
Joshua <swansonj@mail.usf.edu>
2021-03-02 23:23:29 -0500

    Login menu functionality as well as adding service classes that may be needed in the future

---
+++ src/main/java/com/service/UserService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import com.model.UserType;
+
+public class UserService {
+    String username;
+    String password;
+    UserType userType;
+
+
+}
--- src/main/java/com/ui/LoginMenu.java
+++ src/main/java/com/ui/LoginMenu.java
@@ -6,9 +6,15 @@ public class LoginMenu extends AbstractMenu{
 
     @Override
     public void showMenu(Scanner scan) {
-        boolean continueLoop = true;
-        while(continueLoop){
+        for(int i = 0; i < 2; i++){
+            System.out.println("Enter username: ");
+            String username = scan.nextLine();
+            System.out.println("Enter password:");
+            String password = scan.nextLine();
 
+            //TODO: Check database to see if username and password exist
+            //  if they do, check for UserType and redirect to appropriate screen
+            
         }
     }
 }
